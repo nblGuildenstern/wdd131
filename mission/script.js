282ad52e4dfc260a5e95b2c1887cd80681154e2e
nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>
2026-05-02 21:50:01 -0600

    mission 2.1

--- mission/script.js
+++ mission/script.js
@@ -9,23 +9,26 @@ function changeTheme() {
     if (current == 'dark') {
         // code for changes to colors and logo
         const body = document.querySelector('body');
-        body.style.backgroundColor = 'black';
-        body.style.color = 'white';
+        body.classList.add('dark');
 
         const content = document.getElementById('content');
         content.style.borderColor = 'white';
+
+        const h3 = document.querySelector('h3');
+        h3.style.color = `rgb(147, 178, 236)`;
         
         const logo = document.querySelector('img');
         logo.setAttribute('src', 'images/byui-logo-white.png');
     } else {
         // code for changes to colors and logo
         const body = document.querySelector('body');
-        body.style.backgroundColor = 'white';
-        body.style.color = 'black';
-
+        body.classList.remove('dark');
         
         const content = document.getElementById('content');
         content.style.borderColor = 'black';
+        
+        const h3 = document.querySelector('h3');
+        h3.style.color = `rgb(5, 69, 187)`;
 
         const logo = document.querySelector('img');
         logo.setAttribute('src', 'images/byui-logo-blue.png');
