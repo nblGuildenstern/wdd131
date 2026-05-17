852fad3f0970573bb4f4c17d194b9a4fb5dad5e0
nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>
2026-05-16 22:41:59 -0600

    Cool Pics Part 2

--- coolpics/script.js
+++ coolpics/script.js
@@ -37,4 +37,14 @@ modal.addEventListener('keydown', (event) => {
         modal.close();
     }
 });
+
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+}
           
